caef54dd5c7fc24dcd9d344dbc9a269aefabe056
Hassan El Mghari <hassan4709@gmail.com>
2024-05-12 10:48:16 -0700

    another vercel fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse'],
+  },
+};
 
 export default nextConfig;
